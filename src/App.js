539288f8f5026a0caeb472b0dbfb23742e528208
jeremywbrazell <jwbrazell@gmail.com>
2021-03-22 17:35:32 -0700

    pushing

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ class App extends React.Component{
 
   getLocationInfo = async(e) => {
     e.preventDefault();
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.city_explorer}&q=${this.state.searchQuery}&format=json`;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_city_explorer}&q=${this.state.searchQuery}&format=json`;
     const location = await axios.get(url);
     const locationArray = location.data;
     this.setState({ 
       location: locationArray[0], 
       displayResults: true,
-      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.city_explorer}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13` 
+      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_city_explorer}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13` 
     });
   }
 
