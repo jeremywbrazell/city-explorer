844644f6982ac01c65de58746da5b88ce919cd12
jeremywbrazell <jwbrazell@gmail.com>
2021-03-22 17:51:58 -0700

    nsokbnsosb

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ class App extends React.Component{
       imgSrc: '',
       displayResults: false
     }
-    // console.log('constructor');
+
   }
 
   getLocationInfo = async(e) => {
     e.preventDefault();
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_city_explorer}&q=${this.state.searchQuery}&format=json`;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MAP_KEY}&q=${this.state.searchQuery}&format=json`;
     const location = await axios.get(url);
     const locationArray = location.data;
     this.setState({ 
       location: locationArray[0], 
       displayResults: true,
-      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_city_explorer}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13` 
+      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_MAP_KEY}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13` 
     });
   }
 
 
   render() {
-    console.log('state', this.state)
+
     return(
       <>
         <form onSubmit={this.getLocationInfo} >
@@ -39,7 +39,7 @@ class App extends React.Component{
         {this.state.displayResults &&
           <>
             <h2>{this.state.location.display_name}</h2>
-            <img src={this.state.imgSrc} />
+            <img src={this.state.imgSrc} alt='map' title='map'  />
           </>
         }
       </>
