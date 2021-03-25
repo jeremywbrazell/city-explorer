febcaa4e3a88eff59917949b126c6c222db517bd
jeremywbrazell <jwbrazell@gmail.com>
2021-03-24 20:51:29 -0700

    push v5

--- src/forecast.js
+++ src/forecast.js
@@ -10,19 +10,27 @@ class Forecast extends React.Component {
   }
 
   componentDidMount = async() => {
+// for testing purposes
+const lat = 47.6038321;
+const lon = -122.3300624;
+
+
     const SERVER = 'http://localhost:3001';
-    const weather = await axios.get(`${SERVER}/weather`);
-    const forecastArray = weather.data.forecast;
+    const weather = await axios.get(`${SERVER}/weather?lat=${lat}&lon=${lon}`);
+    console.log(weather);
+    const forecastArray = weather.data;
     this.setState({ weatherForecast: forecastArray });
-    console.log(this.state.forecast);
+    console.log(this.state.weatherForecast);
   }
   forecastRender() {
     const data = this.state.weatherForecast;
     console.log('xxxxxx', data);
-      return (<ul>{data.map(item => <li>{item.date}: {item.description}</li>)}</ul>)
+      return (<ul>{data.map((item, index) => <li key={index}>{item.date}: {item.description}</li>)}</ul>)
   }
 
   render() {
+    console.log(this.state)
+    console.log(this.state.weatherForecast);
     return(
       <>
         <h2>Weather</h2>
